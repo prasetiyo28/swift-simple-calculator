93a02a0be87e3911bf42611671f2c983f79f15f0
Bayu Adi Prasetiyo <bayu@leson.id>
2020-08-20 03:51:06 +0700

    make a button for calculate area and reset

--- SimpleCalculator/ViewController.swift
+++ SimpleCalculator/ViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var height: UITextField!
+    @IBOutlet weak var width: UITextField!
+    @IBOutlet weak var result: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func calculateArea(_ sender: Any) {
+        if let h = Double(height.text ?? "0"), let w = Double(width.text ?? "0"){
+            result.text = String(h * w)
+        }else{
+            result.text = "Hasil Tidak Valid"
+        }
+    }
+    
+    @IBAction func resetView(_ sender: Any) {
+        result.text = ""
+        height.text = ""
+        width.text = ""
+    }
 }
 
