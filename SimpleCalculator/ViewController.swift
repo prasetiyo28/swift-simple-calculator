9579fc9c90626f71b751d54a96dd947544ff2ddf
Bayu Adi Prasetiyo <bayu@leson.id>
2020-08-20 03:30:54 +0700

    Initial Commit

---
+++ SimpleCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleCalculator
+//
+//  Created by Bayu Adi Prasetiyo on 20/08/20.
+//  Copyright © 2020 Bayu Adi Prasetiyo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
